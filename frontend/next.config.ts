738a314f2f9c8cd56d08c6a72c5a9c4937c39fd5
suday95 <sudaynandan95@gmail.com>
2026-02-13 23:36:38 +0530

    fix bugs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9000',
+        pathname: '/sessions-media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   async rewrites() {
     return [
